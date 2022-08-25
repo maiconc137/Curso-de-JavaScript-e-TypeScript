84c1328a462ed75c7edd5323064277deb6ad5f92
Tabuada <qumaicon@gmail.com>
2022-08-24 23:41:10 -0300

    finalizando aula7

---
+++ aula7/aula7.js
@@ -0,0 +1,12 @@
+// Não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+const nome ='Maicon';
+
+console.log(nome);
