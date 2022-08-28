49caefc033dc79e7b1a2c5bfacb3991bfc9c3ebc
Tabuada <qumaicon@gmail.com>
2022-08-28 11:43:09 -0300

    mais sobre operadores math

--- aula10/index.js
+++ aula10/index.js
@@ -1,3 +1,11 @@
+/*
++ adição (ou concatenação com strings)
+- subtração
+* multiplicação
+/ divisão
+** potência
+% resto da divisão
+*/
 // NaN - Not a number parseInt (inteiro), parseFloat(decimais)
 const num1 = 10;
 const num2 = Number('Luiz');
