90f8f68dc605eb3b3637921bfdba85324fd3e424
Tabuada <qumaicon@gmail.com>
2022-08-28 12:13:25 -0300

    fim aula10

--- aula10/index.js
+++ aula10/index.js
@@ -5,10 +5,28 @@
 / divisão
 ** potência
 % resto da divisão
+
+++ incrimento
+-- decremento
+
+operadores de atribuição
++=
+-=
+**= ...
 */
+
+/* let contador = 1;
+contador++;
+console.log(contador);
+console.log(++contador);
+console.log(--contador);
+console.log(--contador);
+*/
+
+/*
 // NaN - Not a number parseInt (inteiro), parseFloat(decimais)
 const num1 = 10;
 const num2 = Number('Luiz');
-
 console.log(num1 + num2);
 console.log(typeof num2);
+*/
