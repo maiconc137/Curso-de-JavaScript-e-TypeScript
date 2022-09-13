261fb3651dee3177fdfd02db0e22c00de4aa4716
Tabuada <qumaicon@gmail.com>
2022-09-13 16:25:45 -0300

    reorganizando

--- aula1/index.js
+++
@@ -1,5 +0,0 @@
-console.log('Maicon "Costa"');
-console.log("Maicon 'Costa'");
-console.log(`'Maicon' "Costa" `);
-
-console.log(35, 15.85, 'Maicon Costa de novo')
--- aula5/js/index.js
+++
@@ -1,5 +0,0 @@
-// comentário
-
-console.log('Olá Mundo');
-console.log('Este trecho será exibido no soncole do navegador');
-alert('Olá');
