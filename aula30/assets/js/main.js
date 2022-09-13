f13fa6ef379424e71a19746fa60ef4906e8333a3
Tabuada <qumaicon@gmail.com>
2022-09-13 16:04:30 -0300

    ex02

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -1,13 +1,38 @@
-const data = new Date()
-const hoje = data.getDate();
+const h1 = document.querySelector('.container h1');
+const data = new Date();
 
-console.log(hoje);
-
-switch (key) {
-    case value:
-        
-        break;
-
-    default:
-        break;
+function getDiaSemanaTexto(diaSemana) {
+  const diasSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 
+    'sexta-feira', 'sábado'];
+  return diasSemana[diaSemana];
 }
+
+function getNomeMes(numeroMes) {
+  const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maior', 'junho', 
+    'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+  return meses[numeroMes];
+}
+
+function zeroAEsquerda (num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+function criaData(data) {
+  const diaSemana = data.getDay();
+  const numeroMes = data.getMonth();
+
+  const nomeDia = getDiaSemanaTexto(diaSemana);
+  const nomeMes = getNomeMes(numeroMes);
+
+  return (
+    `${nomeDia}, ${data.getDate()} de ${nomeMes}` + 
+    ` de ${data.getFullYear()} ` + 
+    `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+  );
+}
+
+h1.innerHTML = criaData(data);
+
+// const h1 = document.querySelector('.container h1');
+// const data = new Date();
+// h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short' });
