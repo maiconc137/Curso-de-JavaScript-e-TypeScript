dd96c558c7ff77a5033e11c64f78a1f0911baec7
Tabuada <qumaicon@gmail.com>
2022-09-14 16:49:36 -0300

    exercício com for

---
+++ exercicios/aula35/assets/js/main.js
@@ -0,0 +1,19 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase1'},
+    {tag: 'div', texto: 'Frase2'},
+    {tag: 'section', texto: 'Frase3'},
+    {tag: 'footer', texto: 'Frase4'},
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    let { tag, texto } = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
+    console.log(tag);
+}
+
+container.appendChild(div);
