09ece8a3715b75aafd7b00576ed3eda35bf1a465
Tabuada <qumaicon@gmail.com>
2022-09-14 22:27:19 -0300

    for of

--- notasAulas/aula36/index.js
+++ notasAulas/aula36/index.js
@@ -1,19 +1,30 @@
-// For in -> lê os índices ou chaves do objeto
-//                 0       1      2
 const pessoa = {
-    nome: 'Luiz',
-    sobrenome: 'Otávio',
-    idade: 30
-  };
-  
-  for (let chave in pessoa) {
-    console.log(chave, pessoa[chave]);
-  }
-  
-  // for (let index in frutas) {
-  //   console.log(frutas[index]);
-  // }
-  
-  // for (let i = 0; i < frutas.length; i++) {
-  //   console.log(frutas[i]);
-  // }
+  nome: 'Luiz',
+  sobrenome: 'Otávio'
+};
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
+
+// for (let i = 0; i < nomes.length; i++) {
+//   console.log(nomes[i]);
+// }
+
+// console.log('####');
+
+// for (let i in nomes) {
+//   console.log(nomes[i]);
+// }
+
+// console.log('####');
+
+// for (let valor of nomes) {
+//   console.log(valor);
+// }
+
+// console.log('####');
+
+// nomes.forEach(function(valor, indice, array) {
+//   console.log(valor, indice, array);
+// });
