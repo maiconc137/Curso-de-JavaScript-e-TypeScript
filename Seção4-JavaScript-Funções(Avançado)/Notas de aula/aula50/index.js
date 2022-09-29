7c72c9700a0d369dc62163370983c8ef14f45427
Tabuada <qumaicon@gmail.com>
2022-09-28 22:54:15 -0300

    conhecendo arguments

--- Seção4-JavaScript-Funções(Avançado)/Notas de aula/aula50/index.js
+++ Seção4-JavaScript-Funções(Avançado)/Notas de aula/aula50/index.js
@@ -1,3 +1,12 @@
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
+// argumentos que sustenta todos o sargumentos enviados
+
+function funcao() {
+    let total = 0;
+    for (let argumentos of arguments) {
+        total += argumentos;
+    }
+
+    console.log(total);
 }
+
+funcao(1 ,3 , 6);
