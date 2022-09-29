e76c2975e5a6e1c1a16f00fd3d60c7b1b6661467
Tabuada <qumaicon@gmail.com>
2022-09-28 23:21:42 -0300

    rest operator

--- Seção4-JavaScript-Funções(Avançado)/Notas de aula/aula50/index.js
+++ Seção4-JavaScript-Funções(Avançado)/Notas de aula/aula50/index.js
@@ -1,12 +1,25 @@
 // argumentos que sustenta todos o sargumentos enviados
 
-function funcao() {
-    let total = 0;
-    for (let argumentos of arguments) {
-        total += argumentos;
+// function funcao() {
+//     let total = 0;
+//     for (let argumentos of arguments) {
+//         total += argumentos;
+//     }
+
+//     console.log(total);
+// }
+
+// funcao(1 ,3 , 6);
+
+const conta = function(operador, acumulador, ...numeros) {
+    for (let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '/') acumulador /= numero;
+        if (operador === '*') acumulador *= numero;
     }
 
-    console.log(total);
-}
+    console.log(acumulador);
+};
 
-funcao(1 ,3 , 6);
+conta('+', 0, 20, 30, 40, 50)
