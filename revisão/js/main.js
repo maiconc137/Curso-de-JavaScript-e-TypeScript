f3c9f24e35b7e95d19a569fd27122d1580a6dace
Tabuada <qumaicon@gmail.com>
2022-10-08 08:43:01 -0300

    rev, const

--- revisão/js/main.js
+++ revisão/js/main.js
@@ -7,10 +7,15 @@
 // JavaScript é Case Sentitive
 //  é possível declarar mais de uma variável ao mesmo tempo, ex:
 
-let nome, idade, cidade;
+// let nome, idade, cidade;
 
-nome = 'Maicon';
-idade = 26;
-cidade = 'Poa';
+// nome = 'Maicon';
+// idade = 26;
+// cidade = 'Poa';
 
-console.log(nome, idade, cidade);
+// console.log(nome, idade, cidade);
+
+// o tipo const é para varíaveis constante, quando não pretendemos alterar o valor
+
+//  é uma boa prática usarmos preferencialmente o const, 
+// e o let para quando precisamos ficar alterando os varoles
