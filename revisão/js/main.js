2abf6560b1b87a98975e6a6c2c9a7e259230ebdf
Tabuada <qumaicon@gmail.com>
2022-10-08 08:38:24 -0300

    rev, variáveis

---
+++ revisão/js/main.js
@@ -0,0 +1,16 @@
+// let eu = 'Maicão';
+// console.log(eu)
+
+// camelCase
+// let nomeCompleto = ''
+
+// JavaScript é Case Sentitive
+//  é possível declarar mais de uma variável ao mesmo tempo, ex:
+
+let nome, idade, cidade;
+
+nome = 'Maicon';
+idade = 26;
+cidade = 'Poa';
+
+console.log(nome, idade, cidade);
