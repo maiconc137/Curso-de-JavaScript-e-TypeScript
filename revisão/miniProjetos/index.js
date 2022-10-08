41f9f6f8008f92a6d68608e6508993b126116936
Tabuada <qumaicon@gmail.com>
2022-10-08 14:51:48 -0300

    miniprojeto1

---
+++ revisão/miniProjetos/index.js
@@ -0,0 +1,17 @@
+// troca os valores das variáveis a e b, sem troc literal
+
+let a = 'vermelho';
+let b = 'azul';
+
+console.log(a);
+console.log(b);
+
+console.log('trocando');
+
+let c = a;
+
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
