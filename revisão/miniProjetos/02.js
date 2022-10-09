fc04840146fe07a341ee298ccb602ebb4afca8f5
Tabuada <qumaicon@gmail.com>
2022-10-09 09:31:05 -0300

    update miniprojeto2

--- revisão/miniProjetos/02.js
+++ revisão/miniProjetos/02.js
@@ -1,10 +1,23 @@
 // Escreva uma função que usa 2 números e retorna o maior entre eles
 
+// minha solução
+// function numMax(numUm, numDois) {
+//     if(numUm > numDois) {
+//         console.log(`O número ${numUm} é maior que o ${numDois}`)
+//     } else {
+//         console.log(`O número ${numDois} é maior que o ${numUm}`)
+//     }
+// };
+// numMax(8, 8);
+
+// solução do prof
+
 function numMax(numUm, numDois) {
-    if(numUm > numDois) {
-        console.log(`O número ${numUm} é maior que o ${numDois}`)
-    } else {
-        console.log(`O número ${numDois} é maior que o ${numUm}`)
-    }
-};
-numMax(3, 8);
+    return numUm > numDois ? numUm : numDois;
+    
+    // if (numUm > numDois) {
+    //     return numUm
+    // } return numDois
+}
+
+console.log(numMax(8,3));
