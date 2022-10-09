e53f351514fee23ab882f1eec897e06a9ea2ce00
Tabuada <qumaicon@gmail.com>
2022-10-09 14:44:53 -0300

    miniprojeto9

---
+++ revisão/miniProjetos/09.js
@@ -0,0 +1,10 @@
+// Criar uma função que exibe a quantidade de * que aquela linha possui
+
+exibirAsteriscos(5)
+function exibirAsteriscos(linhas) {
+    let padrao = '';
+    for (let linha = 1; linha <= linhas; linha++) {
+        padrao += '*';
+        console.log(padrao)
+    }
+}
