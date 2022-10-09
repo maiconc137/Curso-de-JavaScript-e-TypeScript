7f760a8f094e8f98214abdc9470b28268a25c574
Tabuada <qumaicon@gmail.com>
2022-10-09 12:26:22 -0300

    update miniprojeto5

--- revisão/miniProjetos/05.js
+++ revisão/miniProjetos/05.js
@@ -1,12 +1,27 @@
 // Receber uma quantidade de valores para avaliar
 // função exibe se cada valor par ou ímpar
-console.log(parImpar(13))
 
-function parImpar(numero) {
-    if (numero % 2 === 0) {
-        console.log(`O número ${numero} é par`)
+// // minha solução
+// console.log(parImpar(13))
+
+// function parImpar(numero) {
+//     if (numero % 2 === 0) {
+//         console.log(`O número ${numero} é par`)
+//     }
+//     else
+//         console.log(`O número ${numero} é ímpar`)
+//     return ''
+// }
+
+// solução professor
+
+exibirTipo(5)
+
+function exibirTipo(limite) {
+    for (let i = 0; i <= limite; i++) {
+        if (i % 2 === 0)
+            console.log(i, 'PAR')
+        else
+        console.log(i, 'ÍMPAR')
     }
-    else
-        console.log(`O número ${numero} é ímpar`)
-    return ''
 }
