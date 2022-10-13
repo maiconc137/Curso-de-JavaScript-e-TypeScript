7aac3f2a9456af5fb16fdaa0b345b259f7e9f2c2
Tabuada <qumaicon@gmail.com>
2022-10-13 20:42:27 -0300

    mais sobre as formas de declaras funções e parâmetros

--- Seção4-JavaScript-Funções(Avançado)/Notas de aula/aula50/index.js
+++ Seção4-JavaScript-Funções(Avançado)/Notas de aula/aula50/index.js
@@ -1,25 +1,54 @@
-// argumentos que sustenta todos o sargumentos enviados
+// arguments = sustenta todos argumento enviados
 
 // function funcao() {
+//     console.log(arguments[0]+' '+arguments[9]);
+// }
+// funcao('Maicon', 4, 6, 19, 8, 9, 2, 9 ,0, 'Costa');
+
+// funcao(4, 6, 19, 8, 9, 2, 9 ,0);
+// function funcao() {
 //     let total = 0;
-//     for (let argumentos of arguments) {
-//         total += argumentos;
+//     for (let argumento of arguments) {
+//         total += argumento;
 //     }
 
-//     console.log(total);
+//     console.log(total)
 // }
 
-// funcao(1 ,3 , 6);
+// function funcao(a,b, c, d, e, f) {
+//     console.log(a,b, c, d, e, f);
+// }
+// funcao(1,2,3);
 
-const conta = function(operador, acumulador, ...numeros) {
-    for (let numero of numeros) {
+// function funcao(a, b = 2, c = 4) {
+//     console.log(a + b + c);
+// }
+// funcao(2, undefined,5);
+
+// function funcao({ nome, sobrenome, idade }) {
+//     console.log(nome, sobrenome, idade);
+// };
+
+// let obj =  {nome: 'Maicon', sobrenome:'Costa', idade: 26}
+// funcao(obj);
+
+// function funcao([ nome, sobrenome, idade ]) {
+//     console.log(nome, sobrenome, idade);
+// };
+
+// let obj =  [ 'Maicon','Costa', 26]
+// funcao(obj);
+
+const conta = function (operador, acumulador, ...numeros) {
+    for (let numero of numeros){
         if (operador === '+') acumulador += numero;
         if (operador === '-') acumulador -= numero;
         if (operador === '/') acumulador /= numero;
         if (operador === '*') acumulador *= numero;
     }
 
-    console.log(acumulador);
+    console.log(acumulador)
 };
+conta('-', 1, 1, 2);
 
-conta('+', 0, 20, 30, 40, 50)
+// argumetns não existe nas arrow functions 
